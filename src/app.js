f02ef8e31c62228ddf90615b3b7d7d36dedbad12
saemauro <maurosaegh@gmail.com>
2023-02-22 12:22:32 -0300

    Agregado login

--- src/app.js
+++ src/app.js
@@ -25,6 +25,10 @@ app.get("/contacto.html", (req,res) => {
 app.get("/register", function(req, res){
     res.sendFile(path.join(__dirname, "views/register.html"))
 })
+
+app.get("/login", function(req, res){
+    res.sendFile(path.join(__dirname, "views/login.html"))
+})
 /*
 app.get("/views", (req,res) => {
     try{
