9c1b3eec456ba84eb66614ad3848f65449bbea13
julieta-bottale <julietabottale@hotmail.com>
2023-03-20 21:05:33 -0300

    detalle de productos dinamico

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -2,6 +2,8 @@ const express = require ("express");
 const app = express();
 const router = express.Router();
 /* const path = require ("path"); */
+const productos = require ("../datosProductos.js");
+
 
 /* app.set ("views",path.join(__dirname, "../views")); */
 
@@ -9,7 +11,10 @@ const controlador = {
 
     detalle_producto : (req,res) => {
         /* res.sendFile(app.get("views")+"/detalle_producto.html"); */   
-        res.render ("detalle_producto"); 
+        
+        let id = req.params.id
+		let producto = productos.find (producto => producto.id == id)
+        res.render ("detalle_producto", {producto}); 
     }, 
     creacion_producto: (req,res) => {
         res.render ("creacion_producto");
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -2,7 +2,9 @@ const express = require ("express");
 const router = express.Router();
 const productosController = require ("../controllers/productosController.js");
 
-router.get("/detalle_producto:idProducto?", productosController.detalle_producto);
+/* router.get("/detalle_producto:idProducto?", productosController.detalle_producto);  */
+
+router.get("/detalle_producto/:id/", productosController.detalle_producto);
 router.get("/creacion_producto", productosController.creacion_producto); 
 router.get("/edicion_producto", productosController.edicion_producto); 
 
