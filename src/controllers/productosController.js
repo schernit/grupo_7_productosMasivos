11b83dc75b9b53bdc41c8b7b592ac25766967167
Sebastian Chernitzky <schernit@gmail.com>
2023-04-04 23:30:19 -0300

    ya funciona el create con multer y ajustamos algunos ejs

--- src/controllers/productosController.js
+++ src/controllers/productosController.js
@@ -1,9 +1,12 @@
 const express = require ("express");
 const app = express();
 const router = express.Router();
+const fs = require("fs");
+const path = require ("path");  
+const productsFilePath = path.join(__dirname, '../data/productos.json');
+
 /* const path = require ("path"); */
 const productos = require ("../datosProductos.js");
-
 
 /* app.set ("views",path.join(__dirname, "../views")); */
 
@@ -20,7 +23,41 @@ const controlador = {
         res.render ("creacion_producto");
     },
     creacion_store : (req,res) => {
-        res.render ("creacion_producto");
+
+        let products = JSON.parse(fs.readFileSync (productsFilePath,"utf-8"));
+
+        //console.log (req.body);
+
+		/* Idea: aprovechar destructuring (...req.body)*/
+
+		/* Crear producto nuevo */
+		/* Para el id, tomar el ultimo elemento, chequear su id y sumarle 1 */
+		
+		let productoNuevo = {
+			id: products[products.length - 1].id + 1,
+            imagen: req.file ? req.file.filename : "default-image.png",
+            nombre: req.body.nombre,
+            descripcion: req.body.descripcion,
+            marca: req.body.marca,
+			precio: parseInt(req.body.precio),
+            moneda: "$",            
+			oferta: parseInt(req.body.oferta),
+			categoria: req.body.categoria,
+			precioDescuentoLeyenda : "$350 la 2da unidad"
+		};
+
+		/* Pushear al array de productos */
+    
+		products.push(productoNuevo);
+        //console.table (products);
+
+		/* Reconvertir a JSON */
+		let productsJSON = JSON.stringify(products, null, " ");
+
+		/* Escribir en el archivo JSON en si */
+		fs.writeFileSync(productsFilePath, productsJSON);
+       
+        res.redirect ("/");
     },
     edicion_producto: (req,res) => {
         res.render ("edicion_producto");
--- src/routes/productos.js
+++ src/routes/productos.js
@@ -1,13 +1,28 @@
 const express = require ("express");
 const router = express.Router();
+const multer = require('multer'); // Requerimos Multer
 const productosController = require ("../controllers/productosController.js");
+const path = require("path");
+
+// ************ Multer config (indicar donde se guarda el archivo, y darle un nombre único) ************
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/img")
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) )
+        /* imagenProducto-3642746274.gif */
+    }
+})
+const upload = multer({storage: storage});
+
 
 // mostrar detalle del producto
 router.get("/detalle_producto/:id/", productosController.detalle_producto);
 
 //creacion de un producto
 router.get("/creacion_producto", productosController.creacion_producto); 
-router.post("/", productosController.creacion_store); 
+router.post("/creacion_producto", upload.single("imagen"),productosController.creacion_store); 
 
 //edicion de un producto
 router.get("/edicion_producto/:id", productosController.edicion_producto); 
