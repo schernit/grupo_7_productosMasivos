de4077d0a5a2e21a02b9a7ec74f3a1d19f759e34
saemauro <maurosaegh@gmail.com>
2023-04-24 17:26:26 -0300

    create de registro agregado

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,9 @@
 const express = require ("express");
 const app = express();
 const router = express.Router();
+const fs = require("fs");
+const path = require ("path");  
+const usersFilePath = path.join(__dirname, '../data/users.json');
 
 
 const controlador = {
@@ -11,6 +14,40 @@ const controlador = {
     
     register : (req,res) => {
         res.render("register");            
+    },
+
+    processRegister: (req,res) => {
+        let users = JSON.parse(fs.readFileSync (usersFilePath,"utf-8"));
+
+        //console.log (req.body);
+
+		/* Idea: aprovechar destructuring (...req.body)*/
+
+		/* Crear producto nuevo */
+		/* Para el id, tomar el ultimo elemento, chequear su id y sumarle 1 */
+		
+		let usuarioNuevo = {
+			id: users[users.length - 1].id + 1,
+            imagen: req.file ? req.file.filename : "default-image.png",
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+			categoria: req.body.categoria,
+            contraseña: req.body.contraseña,            
+		};
+
+		/* Pushear al array de productos */
+    
+		users.push(usuarioNuevo);
+        //console.table (products);
+
+		/* Reconvertir a JSON */
+		let usersJSON = JSON.stringify(users, null, " ");
+
+		/* Escribir en el archivo JSON en si */
+		fs.writeFileSync(usersFilePath, usersJSON);
+       
+        res.redirect ("/");
     }
 
 };
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,25 @@
 const express = require ("express");
 const router = express.Router();
 const usuariosController = require ("../controllers/usersController.js");
+const multer = require('multer'); // Requerimos Multer
+const path = require("path");
+
+// ************ Multer config (indicar donde se guarda el archivo, y darle un nombre único) ************
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, "public/img/usuarios")
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname) )
+        /* imagenProducto-3642746274.gif */
+    }
+})
+const upload = multer({storage: storage});
+
 
 router.get("/register", usuariosController.register)
+router.post("/register",upload.single("imagen") , usuariosController.processRegister)
+
 router.get("/login", usuariosController.login)
 
 module.exports = router;  
