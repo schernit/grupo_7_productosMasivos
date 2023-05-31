ba18df739a434b94884abf0ebcde4c27eabeb52c
julieta-bottale <julietabottale@hotmail.com>
2023-05-31 18:59:34 -0300

    validaciones creacion de producto

--- public/js/creacion_producto.js
+++ public/js/creacion_producto.js
@@ -11,19 +11,39 @@ window.onload = () => {
         //valido nombre
         let nombre = document.querySelector("#nombre");
         if (nombre.value == ""){            
-          errores.push ("el campo nombre está vacio");
+          errores.push ("Se le debe asignar un nombre al producto");
         }
 
-        if (errores.length>0){
+         //valido marca
+        let marca = document.querySelector("#marca");
+        if (marca.value == ""){            
+          errores.push ("Se debe declarar la marca del producto");
+        }
+
+         //valido precio
+        let precio = document.querySelector("#precio");
+        if (precio.value == ""){            
+          errores.push ("Se le debe asignar un precio al producto");
+        }
+     
+        //valido descripcion
+        let descripcion = document.querySelector("#descripcion");
+        if (descripcion.value.length < 20){            
+          errores.push ("La descripción debe contener al menos 20 caracteres");
+        }
+        
+
+    if (errores.length>0){
           event.preventDefault();
           let ulErrores = document.querySelector(".errores ul");
           ulErrores.innerHTML = "<li></li>";
           errores.forEach(error => {
-            ulErrores.innerHTML += "<li>"+error+"</li>";
+            ulErrores.innerHTML += "<li>"+ error+"</li>";
           })
         }
 
       }  
-  };
+    };
   
-  
+   
+        
